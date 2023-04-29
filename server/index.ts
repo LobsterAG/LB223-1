586d72f68c6c6898322b48ca7afdc8fbdf772de8
Denis <denis.basler@gmx.ch>
2023-04-29 17:18:40 +0200

    working template for authentication, proceeding with DBMS and registration

--- server/index.ts
+++ server/index.ts
@@ -12,20 +12,16 @@ class Backend {
   private _api: API
   private _database: Database
   private _env: string
-
   // Getters
   public get app(): Express {
     return this._app
   }
-
   public get api(): API {
     return this._api
   }
-
   public get database(): Database {
     return this._database
   }
-
   // Constructor
   constructor() {
     this._app = express()
@@ -42,19 +38,16 @@ class Backend {
     this.setupRoutes()
     this.startServer()
   }
-
   // Methods
   private setupStaticFiles(): void {
     this._app.use(express.static('client'))
   }
-
   private setupRoutes(): void {
     this._app.get('/', (req: Request, res: Response) => {
       const __dirname = resolve(dirname(''))
       res.sendFile(__dirname + '/client/index.html')
     })
   }
-
   private startServer(): void {
     if (this._env === 'production') {
       http.createServer(this.app).listen(3000, () => {
@@ -63,6 +56,5 @@ class Backend {
     }
   }
 }
-
 const backend = new Backend()
 export const viteNodeApp = backend.app
